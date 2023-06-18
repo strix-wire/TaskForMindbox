555c2ab919d5687fe79bb8c51166f48cd85c5d2b
Vasily Pavlov <strix.wire@gmail.com>
2023-06-18 14:31:00 +0700

    add configuration for future fabric pattern

---
+++ GeometryShape.Application/ShapeConfigurations/CircleConfiguration.cs
@@ -0,0 +1,11 @@
+﻿using GeometryShape.Application.Interfaces;
+using GeometryShape.Application.Shapes;
+
+namespace GeometryShape.Application.ShapeConfigurations;
+
+public class CircleConfiguration : IShapeConfiguration
+{
+    public double Radius { get; set; }
+    public ShapeType ShapeType { get => ShapeType.Circle; }
+}
+
---
+++ GeometryShape.Application/ShapeConfigurations/TriangleConfiguration.cs
@@ -0,0 +1,12 @@
+﻿using GeometryShape.Application.Interfaces;
+using GeometryShape.Application.Shapes;
+
+namespace GeometryShape.Application.ShapeConfigurations;
+
+public class TriangleConfiguration : IShapeConfiguration
+{
+    public double SideA { get; set; }
+    public double SideB { get; set; }
+    public double SideC { get; set; }
+    public ShapeType ShapeType { get => ShapeType.Triangle; }
+}
