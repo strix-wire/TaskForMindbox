549ef3f055e0598bd0b6bc61e4b63a38c8589fa2
Vasily Pavlov <vasily_pavlov_98@mail.ru>
2023-06-18 14:27:56 +0700

    add InvalidTriangleException for not exist triangle

---
+++ GeometryShape.Application/Exceptions/InvalidTriangleException.cs
@@ -0,0 +1,8 @@
+﻿namespace GeometryShape.Application.Exceptions;
+
+public class InvalidTriangleException : Exception
+{
+    public InvalidTriangleException(string message) : base(message)
+    {
+    }
+}
